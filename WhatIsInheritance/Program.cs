0083dec2357f07e2a3bbb6eb180f688c476f09ed
JesseDietrichson <jesse.dietrichson@xamarin.com>
2016-11-29 21:53:15 -0500

    Add project files.

---
+++ WhatIsInheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsInheritance
+{
+    class Program
+    {
+        //Lecture 2: What is Inheritance?
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            e.Name = "Jesse";
+            e.Age = 30;
+            e.ID = "3574dfg5";
+
+
+            e.IntroduceSelf();
+        }
+    }
+}
